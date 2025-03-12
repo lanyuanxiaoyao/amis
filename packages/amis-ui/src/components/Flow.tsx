21caff297645509506ebdbb4f74cdee1af1e6d08
lanyuanxiaoyao <lanyuanxiaoyao@gmail.com>
2025-03-12 23:32:21 +0000

    完成拖拽添加节点

--- packages/amis-ui/src/components/Flow.tsx
+++ packages/amis-ui/src/components/Flow.tsx
@@ -16,31 +16,53 @@ import {
   MiniMap,
   Node,
   NodeTypes,
+  Panel,
   ReactFlow,
+  ReactFlowProvider,
   useEdgesState,
-  useNodesState
+  useNodesState,
+  useReactFlow
 } from '@xyflow/react';
 import {Position, Connection, EdgeBase} from '@xyflow/system';
 import {themeable, ThemeProps} from 'amis-core';
 import {LocaleProps, localeable} from 'amis-core';
 import '@xyflow/react/dist/style.css';
-import {isString} from 'lodash';
+import {isString, uniqueId} from 'lodash';
 
-const DnDContext = React.createContext([null, () => {}]);
-
-const DnDProvider = ({children}: any) => {
-  const [type, setType] = React.useState<any>(null);
-  return <DnDProvider value={[type, setType]}>{children}</DnDProvider>;
-};
-
-const useDnD = () => React.useContext(DnDContext);
-
-const AsideBar: React.FC<{}> = props => {
-  const [type, setType] = useDnD();
+const AsideBar: React.FC<{}> = () => {
+  const onDragStart = (event: any, nodeType: string) => {
+    event.dataTransfer.setData('application/reactflow', nodeType);
+    event.dataTransfer.effectAllowed = 'move';
+  };
 
   return (
     <>
-      <aside></aside>
+      <aside>
+        <div className="description">
+          You can drag these nodes to the pane on the right.
+        </div>
+        <div
+          className="dndnode input"
+          onDragStart={event => onDragStart(event, 'input')}
+          draggable
+        >
+          Input Node
+        </div>
+        <div
+          className="dndnode"
+          onDragStart={event => onDragStart(event, 'default')}
+          draggable
+        >
+          Default Node
+        </div>
+        <div
+          className="dndnode output"
+          onDragStart={event => onDragStart(event, 'output')}
+          draggable
+        >
+          Output Node
+        </div>
+      </aside>
     </>
   );
 };
@@ -129,23 +151,65 @@ const Flow: React.FC<FlowProps> = props => {
     return types;
   }, []);
 
+  const {screenToFlowPosition} = useReactFlow();
+
+  const onDragOver = React.useCallback((event: any) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+  }, []);
+
+  const onDrop = React.useCallback(
+    (event: any) => {
+      event.preventDefault();
+      const type = event.dataTransfer.getData('application/reactflow');
+      const position = screenToFlowPosition({
+        x: event.clientX,
+        y: event.clientY
+      });
+      const newNode: Node = {
+        id: uniqueId('node-'),
+        type,
+        position,
+        data: {label: `${type} node`}
+      };
+
+      setNodes(nds => nds.concat(newNode));
+    },
+    [screenToFlowPosition]
+  );
+
   return (
-    <div className={cx('Flow', className)} style={{height: '50vh'}}>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        nodeTypes={nodeTypes}
-        fitView
-      >
-        {props.controlBar && <Controls />}
-        {props.miniMap && <MiniMap />}
-        <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
-      </ReactFlow>
-    </div>
+    <>
+      <div className={cx('Flow', className)} style={{height: '50vh'}}>
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          nodeTypes={nodeTypes}
+          onDrop={onDrop}
+          onDragOver={onDragOver}
+          fitView
+        >
+          <Panel>
+            <AsideBar />
+          </Panel>
+          {props.controlBar && <Controls />}
+          {props.miniMap && <MiniMap />}
+          <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
+        </ReactFlow>
+      </div>
+    </>
   );
 };
 
-export default themeable(localeable(Flow));
+const FlowProvider: React.FC<FlowProps> = props => {
+  return (
+    <ReactFlowProvider>
+      <Flow {...props} />
+    </ReactFlowProvider>
+  );
+};
+
+export default themeable(localeable(FlowProvider));
